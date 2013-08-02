dd961f3bc153ba6bda7b4c206d6cd6acfa3e9449
Hidenorry <hidenorry@gmail.com>
2013-08-02 20:06:32 +0900

    Modified

--- nmos_idvd.scm
+++ nmos_idvd.scm
@@ -6,9 +6,11 @@
 (load "~/apl/at/gfora.scm")
 (load "~/apl/at/tools.scm")
 
-(define device-path&name "../m3")
+;;(define device-path&name "../m3")
 ;;(define device_name device_path&name)
-(define device-name "m3")
+;;(define device-name "m3")
+
+(define device-path&name-list (map remove-str-extension *argv*))
 
 (define width 1)
 (define temp 300)
@@ -16,15 +18,13 @@
 
 (define vstep 0.05)
 (define coarse-vstep 0.1)
-(define vdrains (list 0 1 2 3 4 5))
-(define vgates (list 0 1 2 3 4 5))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define base-dir "F")
 (define jobtype-list (list 'idvd 'idvg))
 (define vdrains '(0 1 2 3 4 5))
 (define vgates '(0 1 2 3 4 5))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
 
 (define vd-conditions (list 0.05))
 (define vg-conditions (list 0))
@@ -38,44 +38,50 @@
   (% solve vsubstrate= $vbs))
 
 (do-ec
+ (: device-path&name device-path&name-list)
  (: jobtype jobtype-list)
  (: vd vd-conditions)
  (: vg vg-conditions)
  (: vbs vbs-conditions)
  
- (let* ((basedir "")
-        (basedir (add-variables-name basedir "/vd" vd vd-conditions))
-        (basedir (add-variables-name basedir "/vg" vg vg-conditions))
-        (basedir (add-variables-name basedir "/vbs" vbs vbs-conditions)))
-   (sys-system #`"mkdir -p out,|basedir|")
-   (sys-system #`"mkdir -p str,|basedir|")
-     (<main>
-      (<start> go atlas)
-      (% mesh inf= $device-path&name width= $width)
-      (% contact name=gate n.poly)
-      (% models conmob fldmob consrh auger bgn print temp= 300)
-      (% solve init)
-      (% solve prev)
+ (let* ((device-name (sys-basename device-path&name))
+        (basename device-name)
+        (basename (add-variables-name basename "_vd" vd vd-conditions))
+        (basename (add-variables-name basename "_vg" vg vg-conditions))
+        (basename (add-variables-name basename "_vbs" vbs vbs-conditions)))
 
-      (% output e.mobility h.mobility ey.velocity hy.velocity charge)
-      (>> ox.charge e.lines flowlines j.disp j.drift j.diffusion  con.band val.band)
+     (let ((logfile #`"log/,|basename|")
+           (strfile #`"str/,|basename|"))
+       (<main>
+        (<start> go atlas)
+        (% system mkdir -p log)
+        (% system mkdir -p str)
+      
+        (% mesh inf= $device-path&name width= $width)
+        (% contact name=gate n.poly)
+        (% models conmob fldmob consrh auger bgn print temp= 300)
+        (% solve init)
+        (% solve prev)
+
+        (% output e.mobility h.mobility ey.velocity hy.velocity charge)
+        (>> ox.charge e.lines flowlines j.disp j.drift j.diffusion  con.band val.band)
  
-      (% method newton carriers=2 trap maxtrap=15)
+        (% method newton carriers=2 trap maxtrap=15)
 
-      (initialize vd vg vbs)
-      (% save outf= #`"str,|basedir|/init.str")
-      (% log outf= #`"log,|basedir|/result.log")
-      (cond ((eq? jobtype 'idvd)
-             (list
-              (% solve vfinal= (car vdrains) name=drain vstep= $coarse-vstep)
-              (loop vd vdrains
-                    (% solve prev vfinal= $vd name=drain vstep= $vstep)
-                    (% save outf= #`"str,|basedir|/vd,|vd|.str"))))
-            ((eq? jobtype 'idvg)
-             (list
-              (% solve vfinal= (car vgates) name=gate vstep= $coarse-vstep)
-              (loop vg vgates
-                    (% solve prev vfinal= $vg name=gate vstep= $vstep)
-                    (% save outf= #`"str,|basedir|/vd,|vd|_vg,|vg|.str")))))
-      (% log close)
-      (% quit))))
+        (initialize vd vg vbs)
+        (% save outf= #`",|strfile|_init.str")
+        (% log outf= #`",|logfile|.log")
+        (cond ((eq? jobtype 'idvd)
+               (list
+                (% solve vfinal= (car vdrains) name=drain vstep= $coarse-vstep)
+                (loop vd vdrains
+                      (% solve prev vfinal= $vd name=drain vstep= $vstep)
+                      (% save outf= #`",|strfile|_vd,|vd|.str"))))
+              ((eq? jobtype 'idvg)
+               (list
+                (% solve vfinal= (car vgates) name=gate vstep= $coarse-vstep)
+                (loop vg vgates
+                      (% solve prev vfinal= $vg name=gate vstep= $vstep)
+                      (% save outf= #`",|strfile|_vd,|vd|_vg,|vg|.str")))))
+        (% log close)
+        (% quit)))))
